e4dc79aa93a25657d67505ac5836f2b3344e6777
emil <emil.kantaneva@gmail.com>
2023-01-15 20:33:42 +0200

    added script to check DB data

---
+++ data/analyzedata.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# This is a small script to get an idea of the data im working with
+
+# Read the data to a dataframe
+df = pd.read_csv('combined.csv')
+
+# Check for missing values, sanity check
+print("Missing values:")
+print(df.isnull().sum())
+if df.isnull().sum().sum() == 0:
+    print("No missing values")
+
+# Print the number of rows in the dataframe
+print('Combined: ', df.shape[0])
+
+# Get general information about the dataframe
+print("Dataframe info:")
+print(df.info())
+
+# Print the first 5 rows of the dataframe
+print("First 5 rows:")
+print(df.head())
+
+# Print the last 5 rows of the dataframe
+print("Last 5 rows:")
+print(df.tail())
+
+# Describe the dataframe
+print("Dataframe description:")
+print(df.describe())
--- data/preparedata.py
+++ data/preparedata.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# This is a small script to prepare the data for analysis and database insertion
+
 # Read the data to dataframes
 may_2021 = pd.read_csv('2021-05.csv')
 june_2021 = pd.read_csv('2021-06.csv')
@@ -12,4 +14,30 @@ may_2021 = may_2021.dropna()
 june_2021 = june_2021.dropna()
 july_2021 = july_2021.dropna()
 
-# Remove rows with data of trips less than 1 minute
+# Remove rows with data of trips less than 10 seconds
+may_2021 = may_2021[may_2021['Duration (sec.)'] > 10]
+june_2021 = june_2021[june_2021['Duration (sec.)'] > 10]
+july_2021 = july_2021[july_2021['Duration (sec.)'] > 10]
+
+
+# Remove rows with data of trips less than 10 meters
+may_2021 = may_2021[may_2021['Covered distance (m)'] > 10]
+june_2021 = june_2021[june_2021['Covered distance (m)'] > 10]
+july_2021 = july_2021[july_2021['Covered distance (m)'] > 10]
+
+# Print the number of rows in each dataframe
+
+print('May 2021: ', may_2021.shape[0])
+print('June 2021: ', june_2021.shape[0])
+print('July 2021: ', july_2021.shape[0])
+
+# Combine the dataframes
+df = pd.concat([may_2021, june_2021, july_2021])
+
+# Print the number of rows in the combined dataframe
+print('Combined: ', df.shape[0])
+
+# Save the combined dataframe to a csv file
+df.to_csv('combined.csv', index=False)
+
+
