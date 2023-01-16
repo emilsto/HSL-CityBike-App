91d127d9ff685256429dc99fea23cb267d9ca8c4
emil <emil.kantaneva@gmail.com>
2023-01-16 17:35:51 +0200

    added station.csv handling for sql init and parse_data.py

---
+++ data/parse_data.py
@@ -0,0 +1,65 @@
+import pandas as pd
+
+# This is a small script to prepare the data for analysis and database insertion
+
+# Read the data to dataframes
+may_2021 = pd.read_csv('2021-05.csv')
+june_2021 = pd.read_csv('2021-06.csv')
+july_2021 = pd.read_csv('2021-07.csv')
+locations = pd.read_csv('locations.csv')
+
+
+# Verifying and cleaning the data
+
+# Remove the rows with missing values
+may_2021 = may_2021.dropna()
+june_2021 = june_2021.dropna()
+july_2021 = july_2021.dropna()
+
+# Remove rows with data of trips less than 10 seconds
+may_2021 = may_2021[may_2021['Duration (sec.)'] > 10]
+june_2021 = june_2021[june_2021['Duration (sec.)'] > 10]
+july_2021 = july_2021[july_2021['Duration (sec.)'] > 10]
+
+
+# Remove rows with data of trips less than 10 meters
+may_2021 = may_2021[may_2021['Covered distance (m)'] > 10]
+june_2021 = june_2021[june_2021['Covered distance (m)'] > 10]
+july_2021 = july_2021[july_2021['Covered distance (m)'] > 10]
+
+# Print the number of rows in each dataframe
+
+print('May 2021: ', may_2021.shape[0])
+print('June 2021: ', june_2021.shape[0])
+print('July 2021: ', july_2021.shape[0])
+
+# Combine the dataframes
+df = pd.concat([may_2021, june_2021, july_2021])
+
+# Edit the column names of the combined dataframe
+df.columns = ['departure', 'return', 'departure_station_id', 'departure_station_name', 'return_station_id', 'return_station_name', 'distance_m', 'duration_sec']
+
+# Delete first 2 columns of the locations dataframe
+locations = locations.drop(locations.columns[[0, 1]], axis=1)
+
+# Delete colums "Stad" and "Operaattor", they are not needed
+locations = locations.drop(locations.columns[[6, 7]], axis=1)
+
+#print column names
+print(locations.columns)
+
+# Rename the columns of the locations dataframe
+locations.columns = ['name_fi', 'name-se', 'name', 'address', 'address_se', 'city', 'capacity', 'latitude', 'longitude']
+
+# Print 10 random rows of the locations dataframe
+print(locations.sample(10))
+
+# Save the locations dataframe to a csv file
+locations.to_csv('../db/stations.csv', index=False)
+
+
+# Print the number of rows in the combined dataframe
+print('Combined: ', df.shape[0])
+
+# Save the combined dataframe to a csv file
+df.to_csv('../db/bikedata.csv', index=False)
--- db/initdb.sql
+++ db/initdb.sql
@@ -16,11 +16,27 @@ CREATE TABLE hsl_schema.trips (
     duration_sec INT NOT NULL
 );
 
+CREATE TABLE hsl_schema.stations (
+    id SERIAL PRIMARY KEY,
+    name_fi VARCHAR(255) NOT NULL,
+    name_sv VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    address_sv VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    capacity INT NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
+);
+
+
 ALTER DATABASE bikedata OWNER TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE bikedata TO postgres;
 
 COPY hsl_schema.trips (departure, return, departure_station_id, departure_station_name, return_station_id, return_station_name, distance_m, duration_sec) FROM '/bikedata.csv' DELIMITER ',' CSV HEADER;
 
+COPY hsl_schema.stations (name_fi, name_sv, name, address, address_sv, city, capacity, latitude, longitude) FROM '/stations.csv' DELIMITER ',' CSV HEADER;
+
 select * from hsl_schema.trips limit 10;
 
-\dt;
+select * from hsl_schema.stations limit 10;
